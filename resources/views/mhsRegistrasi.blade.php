55b50985c8e7fc9a2f8dce9399c45061a385caa9
Syariful Hanif Setiawan <syarifulhanif@gmail.com>
2024-09-16 18:48:46 +0700

    make header for all page

--- resources/views/mhsRegistrasi.blade.php
+++ resources/views/mhsRegistrasi.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css','resources/js/app.js', 'resources/js/darkmode.js'])
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&family=Poppins:wght@200;400&display=swap"
-    rel="stylesheet">
-    <title>Document</title>
-</head>
-<body class = "bg-gray-100 dark:bg-blek-900">
+@extends('header')
+
+@section('title','Registrasi')
+
+@section('page')
      {{-- navbar --}}
      <x-navbar></x-navbar>
     
@@ -53,5 +46,6 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+
+
+@endsection
